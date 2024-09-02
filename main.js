3a269db8438e211f6e7f1f9ece3ed2f5ab466ebc
karyulla <163071158+karyulla@users.noreply.github.com>
2024-09-02 19:51:14 -0300

    020924

--- main.js
+++ main.js
@@ -10,63 +10,37 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Processos naturais",
-                afirmacao: " Atividades como erupções vulcânicas, tempestades de poeira e liberação de gases pelos oceanos, também contribuem para a poluição do ar."
+                afirmacao: "Atividades como erupções vulcânicas, tempestades de poeira e liberação de gases pelos oceanos, também contribuem para a poluição do ar."
             },
             {
-                texto: "Emissão de veículos",
-                afirmacao: "Carros, caminhões, motos e outros veículos que queimam combustíveis fósseis liberam gases."
+                texto: "Processos humanos",
+                afirmacao: "A poluição atmosférica causada por atividades humanas vem principalmente da queima de combustíveis fósseis, indústrias, agricultura e desmatamento, liberando poluentes que prejudicam a saúde e o meio ambiente."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado:"Como a poluição atmosférica afeta na saúde humana? "
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: " Doenças respiratórias e cardiovasculares"
+                afirmacao: "A inalação de poluentes como material particulado pode causar ou agravar doenças respiratórias, como asma e DPOC, E aumentar os riscos de problemas cardiovasculares,como ataques cardíacos e derrames."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
+                texto: "Impacto no sistema nervoso central"
+                afirmacao: "A esposição prolongada a poluentes pode causar danos ao cérebro, levando a declínio cognitivo, aumento de risco de doenças neurodegenerativas como alzheimer e parkinson, e transtorno de desenvolvimento em crianças." 
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Quais são os efeitos da poluição atmosférica no meio ambiente?"
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Alteração climática"
+                afirmacao: "Poluentes como CO2 e metano contribuem para o aquecimento global, causando mudanças climáticas e impactando ecossistemas."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Acidificação dos ecossistemas"
+                afirmacao: "Gases como SO2 e NOx formam chuva ácida, que podem danificar solos, corpos d'água e vegetação."
+          
             }
         ]
     },
